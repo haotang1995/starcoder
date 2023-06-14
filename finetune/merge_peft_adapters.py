7e1e2050f7d73c78a8099cea0cfc72356b161f0b
Hao Tang <tanghaosjtu@gmail.com>
2023-06-14 13:48:50 -0400

    minor debug

--- finetune/merge_peft_adapters.py
+++ finetune/merge_peft_adapters.py
@@ -8,7 +8,7 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--base_model_name_or_path", type=str, default="bigcode/large-model")
-    parser.add_argument("--output_model_name_or_path", type=str, default="./checkpoints")
+    parser.add_argument("--output_model_name_or_path", type=str, default=None)
     parser.add_argument("--peft_model_path", type=str, default="/")
     parser.add_argument("--push_to_hub", action="store_true", default=False)
 
@@ -16,6 +16,10 @@ def get_args():
 
 def main():
     args = get_args()
+    if args.output_model_name_or_path is None:
+        # Remove / in the end of peft_model_path
+        args.output_model_name_or_path = args.peft_model_path.rstrip("/")
+        print(f"Output model name or path not specified, using {args.output_model_name_or_path}")
 
     base_model = AutoModelForCausalLM.from_pretrained(
         args.base_model_name_or_path,
