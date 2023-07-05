500c5f451c8c45b3010b0adfdb7f904746279fb6
Hao Tang <tanghaosjtu@gmail.com>
2023-07-05 16:44:34 -0400

    Add more finetuning settings

---
+++ finetune/finetune_mbpp_refinement_1step.py
@@ -0,0 +1,401 @@
+# @Hao @Jun 13, 2023
+# Copied from Starcoder/finetune/finetuen.py
+# Modified to make 'train' == 'test' for the purpose of debugging
+import argparse
+import os
+
+import torch
+from accelerate import Accelerator
+from datasets import load_dataset, concatenate_datasets
+from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, set_peft_model_state_dict
+from peft import PeftConfig, PeftModel
+from torch.utils.data import IterableDataset
+from tqdm import tqdm
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, logging, set_seed
+from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl
+from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+
+"""
+Fine-Tune StarCoder on Code Alpaca/SE
+"""
+
+from transformers.trainer import logger, is_sagemaker_mp_enabled, load_sharded_checkpoint, get_last_checkpoint
+class MyTrainer(Trainer):
+    def _load_from_checkpoint(self, resume_from_checkpoint, model=None):
+        return
+
+class SavePeftModelCallback(TrainerCallback):
+    def on_save(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        checkpoint_folder = os.path.join(args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}")
+        print(f"Saving peft model to {checkpoint_folder}.")
+
+        kwargs["model"].save_pretrained(checkpoint_folder)
+
+        pytorch_model_path = os.path.join(checkpoint_folder, "pytorch_model.bin")
+        torch.save({}, pytorch_model_path)
+        return control
+
+
+class LoadBestPeftModelCallback(TrainerCallback):
+    def on_train_end(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        print(f"Loading best peft model from {state.best_model_checkpoint} (score: {state.best_metric}).")
+        # if state.best_model_checkpoint is None:
+            # print("Error: No best model checkpoint found. Skipping.")
+            # return control
+        best_model_path = os.path.join(state.best_model_checkpoint, "adapter_model.bin")
+        adapters_weights = torch.load(best_model_path)
+        model = kwargs["model"]
+        set_peft_model_state_dict(model, adapters_weights)
+        return control
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default="bigcode/large-model")
+    parser.add_argument("--dataset_name", type=str, default="HuggingFaceH4/CodeAlpaca_20K")
+    parser.add_argument("--subset", type=str)
+    parser.add_argument("--split", type=str)
+    parser.add_argument("--size_valid_set", type=int, default=10000)
+    parser.add_argument("--streaming", action="store_true")
+    parser.add_argument("--shuffle_buffer", type=int, default=5000)
+
+    parser.add_argument("--input_column_name", type=str, default="prompt")
+    parser.add_argument("--output_column_name", type=str, default="completion")
+
+    parser.add_argument("--seq_length", type=int, default=2048)
+    parser.add_argument("--max_steps", type=int, default=10000)
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=16)
+    parser.add_argument("--eos_token_id", type=int, default=49152)
+
+    parser.add_argument("--lora_r", type=int, default=16)
+    parser.add_argument("--lora_alpha", type=int, default=32)
+    parser.add_argument("--lora_dropout", type=float, default=0.05)
+
+    parser.add_argument("--learning_rate", type=float, default=5e-6)
+    parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
+    parser.add_argument("--num_warmup_steps", type=int, default=100)
+    parser.add_argument("--weight_decay", type=float, default=0.05)
+
+    parser.add_argument("--local-rank", type=int, default=0)
+    parser.add_argument("--no_fp16", action="store_false")
+    parser.add_argument("--bf16", action="store_true", default=True)
+    parser.add_argument("--no_gradient_checkpointing", action="store_false", default=False)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--num_workers", type=int, default=None)
+    parser.add_argument("--output_dir", type=str, default="./checkpoints")
+    parser.add_argument("--log_freq", default=100, type=int)
+    parser.add_argument("--eval_freq", default=100, type=int)
+    parser.add_argument("--save_freq", default=1000, type=int)
+
+    parser.add_argument('--resume_from_checkpoint', type=str, default=True)
+
+    return parser.parse_args()
+
+
+def chars_token_ratio(dataset, tokenizer, input_column_name="prompt", output_column_name="completion", nb_examples=400):
+    """
+    Estimate the average number of characters per token in the dataset.
+    """
+    total_characters, total_tokens = 0, 0
+    for _, example in tqdm(zip(range(nb_examples), iter(dataset)), total=nb_examples):
+        text = prepare_sample_text(example, input_column_name, output_column_name)
+        total_characters += len(text)
+        if tokenizer.is_fast:
+            total_tokens += len(tokenizer(text).tokens())
+        else:
+            total_tokens += len(tokenizer.tokenize(text))
+
+    return total_characters / total_tokens
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+def prepare_sample_text(example, input_column_name="prompt", output_column_name="completion"):
+    """Prepare the text from a sample of the dataset."""
+    # text = f"{example[input_column_name]}\n{example[output_column_name]}"
+    text = f"<commit_start>{example[args.input_column_name].strip()}<commit_msg>Debugging<commit_after>{example[args.output_column_name].strip()}<eos>"
+    return text
+
+
+class ConstantLengthDataset(IterableDataset):
+    """
+    Iterable dataset that returns constant length chunks of tokens from stream of text files.
+        Args:
+            tokenizer (Tokenizer): The processor used for proccessing the data.
+            dataset (dataset.Dataset): Dataset with text files.
+            infinite (bool): If True the iterator is reset after dataset reaches end else stops.
+            seq_length (int): Length of token sequences to return.
+            num_of_sequences (int): Number of token sequences to keep in buffer.
+            chars_per_token (int): Number of characters per token used to estimate number of tokens in text buffer.
+    """
+
+    def __init__(
+        self,
+        tokenizer,
+        dataset,
+        infinite=False,
+        seq_length=1024,
+        num_of_sequences=1024,
+        chars_per_token=3.6,
+        input_column_name="prompt",
+        output_column_name="completion"
+    ):
+        self.tokenizer = tokenizer
+        self.concat_token_id = tokenizer.eos_token_id if tokenizer.eos_token_id is not None else args.eos_token_id
+        self.dataset = dataset
+        self.seq_length = seq_length
+        self.infinite = infinite
+        self.current_size = 0
+        self.max_buffer_size = seq_length * chars_per_token * num_of_sequences
+        self.input_column_name = input_column_name
+        self.output_column_name = output_column_name
+
+    def __iter__(self):
+        iterator = iter(self.dataset)
+        more_examples = True
+        while more_examples:
+            buffer, buffer_len = [], 0
+            while True:
+                if buffer_len >= self.max_buffer_size:
+                    break
+                try:
+                    buffer.append(prepare_sample_text(next(iterator), self.input_column_name, self.output_column_name))
+                    buffer_len += len(buffer[-1])
+                except StopIteration:
+                    if self.infinite:
+                        iterator = iter(self.dataset)
+                    else:
+                        more_examples = False
+                        break
+            tokenized_inputs = self.tokenizer(buffer, truncation=False)["input_ids"]
+            all_token_ids = []
+            for tokenized_input in tokenized_inputs:
+                all_token_ids.extend(tokenized_input + [self.concat_token_id])
+            for i in range(0, len(all_token_ids), self.seq_length):
+                input_ids = all_token_ids[i : i + self.seq_length]
+                if len(input_ids) == self.seq_length:
+                    self.current_size += 1
+                    yield {
+                        "input_ids": torch.LongTensor(input_ids),
+                        "labels": torch.LongTensor(input_ids),
+                    }
+
+def mbpp_preprocess(example):
+    prompt = example["prompt"]
+    code = example["code"]
+    test_list = example["test_list"]
+    test_imports = example["test_imports"]
+
+    func_name_list = [
+        c[4:].split('(')[0].strip()
+        for c in code.split('\n')
+        if 'def ' == c[:4]
+    ]
+    func_name = [
+        fn
+        for fn in func_name_list
+        if all([fn in test for test in test_list])
+    ]
+    assert(len(func_name) == 1), f"func_name: {func_name}, func_name_list: {func_name_list}, test_list: {test_list}"
+    func_name = func_name[0]
+
+    code_blocks = [[],]
+    for c in code.split('\n'):
+        if c.startswith('def '):
+            code_blocks.append([])
+        code_blocks[-1].append(c)
+    test_imports += [c for c in code_blocks[0] if c.startswith('import ') or c.startswith('from ')]
+    code_blocks = code_blocks[1:]
+    assert(all([c[0].startswith('def ') for c in code_blocks]))
+
+    func_def = [c for c in code.split('\n') if f'def {func_name}(' in c or f'def {func_name} (' in c]
+    assert(len(func_def) == 1), f"func_def: {func_def}, func_name: {func_name}, code: {code}"
+    func_def = func_def[0]
+
+    func_def_index = [cbi for cbi, cb in enumerate(code_blocks) if func_def.strip() == cb[0].strip()]
+    assert(len(func_def_index) == 1), f"func_def_index: {func_def_index}, func_def: {func_def}, code_blocks: {code_blocks}"
+    func_def_index = func_def_index[0]
+    code_blocks = code_blocks[:func_def_index] + code_blocks[func_def_index+1:] + [code_blocks[func_def_index][1:]]
+
+    main_cb_line = code_blocks[-1][0]
+    indent = main_cb_line[:len(main_cb_line) - len(main_cb_line.lstrip())]
+
+    assert(all([test.strip().startswith('assert') for test in test_list]))
+    running_examples = [test.strip()[6:].strip() for test in test_list]
+
+    new_prompt = '\n'.join(test_imports) + '\n'
+    new_prompt += func_def + '\n'
+    new_prompt += indent+'"""\n'
+    new_prompt += '\n'.join([indent+p for p in prompt.split('\n')]) + '\n'
+    new_prompt += '\n'.join([indent+'>>> '+p for p in running_examples]) + '\n'
+    new_prompt += indent+'"""\n'
+
+    new_code = '\n'.join(['\n'.join([indent+c for c in cb]) for cb in code_blocks[:-1]])
+    new_code += '\n'.join(code_blocks[-1])
+
+    example["prompt"] = new_prompt
+    example["code"] = new_code
+    return example
+
+
+def create_datasets(tokenizer, args):
+    # dataset = load_dataset(
+        # 'mbpp',
+        # 'sanitized',
+        # use_auth_token=True,
+    # )
+    # dataset = dataset.map(mbpp_preprocess,)
+    # dataset['train'] = concatenate_datasets([dataset['train'], dataset['validation'], dataset['test'], dataset['prompt']])
+    dataset = load_dataset('json', data_files={'test': args.dataset_name}, split=args.split, streaming=args.streaming)
+    dataset_len = len(dataset['test'])
+    split_index = int(0.9 * dataset_len)
+    dataset['test'] = dataset['test'].shuffle(seed=args.seed)
+    dataset['train'] = dataset['test'].select(range(split_index))
+    dataset['test'] = dataset['test'].select(range(split_index, dataset_len))
+    if args.streaming:
+        print("Loading the dataset in streaming mode")
+        valid_data = dataset.take(args.size_valid_set)
+        train_data = dataset.skip(args.size_valid_set)
+        train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
+    else:
+        train_data = dataset["train"]
+        valid_data = dataset["test"]
+        print(f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}")
+
+    chars_per_token = chars_token_ratio(train_data, tokenizer, args.input_column_name, args.output_column_name)
+    print(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
+
+    train_dataset = ConstantLengthDataset(
+        tokenizer,
+        train_data,
+        infinite=True,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+        input_column_name=args.input_column_name,
+        output_column_name=args.output_column_name
+    )
+    valid_dataset = ConstantLengthDataset(
+        tokenizer,
+        valid_data,
+        infinite=False,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+        input_column_name=args.input_column_name,
+        output_column_name=args.output_column_name
+    )
+    return train_dataset, valid_dataset
+
+
+def run_training(args, train_data, val_data):
+    print("Loading the model")
+    # disable caching mechanism when using gradient checkpointing
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_path,
+        use_auth_token=True,
+        use_cache=not args.no_gradient_checkpointing,
+        load_in_8bit=True,
+        device_map={"": Accelerator().process_index},
+    )
+    model = prepare_model_for_int8_training(model)
+
+    if args.resume_from_checkpoint:
+        if isinstance(args.resume_from_checkpoint, bool):
+            args.resume_from_checkpoint = get_last_checkpoint(args.output_dir)
+
+    if not args.resume_from_checkpoint:
+        lora_config = LoraConfig(
+            r=args.lora_r,
+            lora_alpha=args.lora_alpha,
+            lora_dropout=args.lora_dropout,
+            bias="none",
+            task_type="CAUSAL_LM",
+            target_modules = ["c_proj", "c_attn", "q_attn"]
+        )
+        model = get_peft_model(model, lora_config)
+    else:
+        print(f"Resuming from checkpoint: {args.resume_from_checkpoint}")
+        model = PeftModel.from_pretrained(model, args.resume_from_checkpoint, is_trainable=True)
+        model.train()
+
+    print_trainable_parameters(model)
+
+    train_data.start_iteration = 0
+
+    print("Starting main loop")
+
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        dataloader_drop_last=True,
+        evaluation_strategy="steps",
+        max_steps=args.max_steps,
+        eval_steps=args.eval_freq,
+        save_steps=args.save_freq,
+        logging_steps=args.log_freq,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        lr_scheduler_type=args.lr_scheduler_type,
+        warmup_steps=args.num_warmup_steps,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        gradient_checkpointing=not args.no_gradient_checkpointing,
+        fp16=not args.no_fp16,
+        bf16=args.bf16,
+        weight_decay=args.weight_decay,
+        run_name="StarCoder-remember",
+        report_to="wandb",
+        ddp_find_unused_parameters=False,
+        load_best_model_at_end=True,
+        resume_from_checkpoint=args.resume_from_checkpoint,
+    )
+
+    trainer = MyTrainer(model=model, args=training_args, train_dataset=train_data, eval_dataset=val_data, callbacks=[SavePeftModelCallback, LoadBestPeftModelCallback])
+
+    print("Training...")
+    trainer.train(resume_from_checkpoint=args.resume_from_checkpoint)
+
+    print("Saving last checkpoint of the model")
+    model.save_pretrained(os.path.join(args.output_dir, "final_checkpoint/"))
+
+
+def main(args):
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, use_auth_token=True)
+    train_dataset, eval_dataset = create_datasets(tokenizer, args)
+    run_training(args, train_dataset, eval_dataset)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    print(args)
+
+    set_seed(args.seed)
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    logging.set_verbosity_error()
+
+    main(args)
